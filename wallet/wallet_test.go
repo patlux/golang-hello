b80aa465a3b37fc5be4f5b79b148d49977d30b82
Patrick Wozniak <email@patwoz.de>
2021-03-20 23:01:41 +0100

    feat(wallet): adding withdraw

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -5,14 +5,33 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	got := wallet.Balance()
-	want := Bitcoin(10)
+		wallet.Deposit(10)
 
-	if got != want {
-		t.Errorf("Got %s want %s", got, want)
-	}
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("Got %s want %s", got, want)
+		}
+
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: 15}
+
+		wallet.Withdraw(10)
+
+		got := wallet.Balance()
+		want := Bitcoin(5)
+
+		if got != want {
+			t.Errorf("Got %s want %s", got, want)
+		}
+
+	})
+
 }
