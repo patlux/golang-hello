c451b3c29fab77a6afdce67539af77fa7d5ff2a8
Patrick Wozniak <email@patwoz.de>
2021-03-20 11:59:00 +0100

    feat: make use of variadic functions to take an arbitrary number of args

--- sum.go
+++ sum.go
@@ -1,21 +1,28 @@
 package main
 
 func Sum(arr []int) (sum int) {
-
 	for _, nr := range arr {
 		sum += nr
 	}
-
 	return
 }
 
-func SumAll(arr, arr2 []int) (sumarr []int) {
-	all := [][]int{arr, arr2}
-
-	for _, arr := range all {
-		result := Sum(arr)
-		sumarr = append(sumarr, result)
+func SumAll(listOfArrNumbers ...[]int) (sumarr []int) {
+	for _, arrNumbers := range listOfArrNumbers {
+		sumarr = append(sumarr, Sum(arrNumbers))
 	}
-	
+	return
+}
+
+func SumAllTails(listOfArrNumbers ...[]int) (sumarr []int) {
+	for _, arrNumbers := range listOfArrNumbers {
+		if len(arrNumbers) == 0 {
+			sumarr = append(sumarr, 0)
+			continue
+		}
+
+		lastNumbers := arrNumbers[1:]
+		sumarr = append(sumarr, Sum(lastNumbers))
+	}
 	return
 }
